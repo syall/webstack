6fca810603e089ea4711ac7b8e511ac7dda53e00
syall <s.yuan.all@gmail.com>
2020-03-16 22:25:17 -0400

    Use template engine

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const SimpleServer = require('./src/SimpleServer');
 const SimpleRouter = require('./src/SimpleRouter');
+const render = require('./src/render');
 const { handleSignals } = require('./src/utils');
 
 // Dot Env
@@ -16,14 +17,17 @@ router.add('^/(health|ping)$', (_, res) => res.end());
 
 // Index Route
 router.add('^/$', (req, res) => res
-    .writeHead(200, { 'Content-Type': 'application/json' })
-    .end(req.data.length === 0
-        ? 'no data'
-        : JSON.stringify(JSON.parse(req.data))));
-
+    .writeHead(200, { 'Content-Type': 'text/html' })
+    .end(render('index.html', {
+        data: req.data ? JSON.parse(req.data) : {}
+    }))
+);
 
 // Not Found
-router.add('/', (_, res) => res.writeHead(404).end());
+router.add('/', (_, res) => res
+    .writeHead(404, { 'Content-Type': 'text/html' })
+    .end(render('404.html'))
+);
 
 // Run Server
 server.run();
